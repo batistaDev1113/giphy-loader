1a91a73d18cbb36dbface5ada131ded86c113cd5
yundeveloper <yuniorbatista1113@gmail.com>
2021-02-08 13:25:19 -0500

    Updates

---
+++ precache-manifest.2fa3597bcb702cf3fb37ff51bb4455fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599bba0d209d4f5903c61c19900ededf",
+    "url": "/giphy-loader/index.html"
+  },
+  {
+    "revision": "e99875fd89033a659a36",
+    "url": "/giphy-loader/static/css/main.7a0e6566.chunk.css"
+  },
+  {
+    "revision": "5d985b301d5ae01a4bbb",
+    "url": "/giphy-loader/static/js/2.ed5ecfce.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/giphy-loader/static/js/2.ed5ecfce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99875fd89033a659a36",
+    "url": "/giphy-loader/static/js/main.f53ecf26.chunk.js"
+  },
+  {
+    "revision": "fb73e74fc5175ccb6793",
+    "url": "/giphy-loader/static/js/runtime-main.a81a5e75.js"
+  }
+]);
